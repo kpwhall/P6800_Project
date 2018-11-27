9b0d87061072475da15baeb397d5f0c053f628f2
Kyle Hall <kpwh23@mun.ca>
2018-11-27 16:13:05 -0330

    Small changes to main.py

--- main.py
+++ main.py
@@ -9,11 +9,15 @@ SPACE=204   # Space group being considered
 ge = csvRead("./data/ge/ge"+str(SPACE)+".csv", ['ITA', 'rot', 'trans'])
 wp = csvRead("./data/wp/wp"+str(SPACE)+".csv", ['Mult', 'Letter', 'Symm', 'Pos'])
 
-rots, trans=[], []
+#Get transforms. For the purposes of this program, one per class is all that's needed, so we can disregard the rest.
+transforms=[]
+classList=[]
 for x in ge:
-    rots.append(Matrix(sympify(x[1])))
-    trans.append(Matrix(sympify(x[2])))
+    if (not x[0].split()[0] in classList):
+        classList.append(x[0].split()[0])
+        transforms.append([Matrix(sympify(x[1])),Matrix(sympify(x[2])),x[0]])
 
+#Get all Wyckoff positions. May easily be changed to take only a selected Wyckoff position if required.
 pos, ptitle=[],[]
 i=0
 for x in wp:
@@ -25,6 +29,44 @@ for x in wp:
         pos[i].append(Matrix(sympify(z)))
     i+=1
 
+# Atomic character
+i=0
+achar=[]
 for x in pos:
-    print x
+    achar.append([])
+    for t in transforms:
+        count=0
+        for y in x:
+            if t[0]*y+t[1]==y:
+                count+=1
+        achar[i].append(count)
+    i+=1
+    
+
+# Displacement character
+vec=Matrix(sympify("[[x],[y],[z]]"))
+dchar=[]
+i=0
+for t in transforms:
+    count=0
+    tvec=t[0]*vec+t[1]
+    for i in range(3):
+        count += 1 if tvec[i]==vec[i] else 0
+        count -= 1 if tvec[i]==-vec[i] else 0
+    dchar.append(count)
+
+#characters
+char=[]
+for i in range(len(ptitle)):
+    print ptitle[i]
+    char.append([])
+    for j in range(len(achar[i])):
+        char[i].append(achar[i][j]*dchar[j])
+    print char[i]
     print
+
+myset=[]
+for i in transforms:
+    myset.append(i[2].split()[0])
+
+print list(set(myset))
