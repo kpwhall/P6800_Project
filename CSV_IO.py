7cdd1e6ac262a4bd58d510e2bbcc24fe20d46691
Kyle Hall <kpwh23@mun.ca>
2018-11-27 13:27:30 -0330

    Main updates 	- CSV_IO now only reads in data and returns it raw. Conversions must be done 	on returned data afterwards. 	- Main now has basic conversion functionality.

--- CSV_IO.py
+++ CSV_IO.py
@@ -1,8 +1,6 @@
 import csv
-import numpy as np
-
-test=[{'ITA': '1', 'rot': np.array([[1,0,0],[0,1,0],[0,0,1]]), 'trans': np.array([[0],[0],[0]])}, \
-        {'ITA': '2 (0,0,1/2) 1/4,0,z ', 'rot': np.array([[-1,0,0],[0,-1,0],[0,0,1]]), 'trans': np.array([[1/2.],[0],[1/2.]])}]
+from sympy import symbols, sympify
+from sympy.matrices import Matrix
 
 def csvWrite(filename, fieldnames, data):
     with open(filename, 'wb') as csvfile:
@@ -13,12 +11,10 @@ def csvWrite(filename, fieldnames, data):
 
 
 def csvRead(filename, fieldnames):
-        ge=[]
+        data=[]
         with open(filename) as csvfile:
                 reader = csv.DictReader(csvfile)
                 for row in reader:
-                        a=np.fromstring(row['rot'].replace("\n","").replace("[","").replace("]","").replace(",",""), sep=" ").reshape(3,3)
-                        b=np.fromstring(row['trans'].replace("\n","").replace("[","").replace("]","").replace(",",""), sep=" ")
-                        ge.append((a,b))
-        return ge
+                        data.append(map(lambda x: row[x], fieldnames))
+        return data
                         
--- main.py
+++ main.py
@@ -1,8 +1,30 @@
 from CSV_IO import csvRead
-import numpy as np
+from sympy import symbols, sympify
+from sympy.matrices import Matrix
 
-SPACE=204
-K=[0,0,0]
-POS=[np.array([[0],[0],[0]]),np.array([[1./2],[1./2],[1./2]])]
+# Parameter definitions ############################
+SPACE=204   # Space group being considered
 
-print csvRead("./data/ge/ge"+str(SPACE)+".csv", ['ITA', 'rot', 'trans'])
+# Collect data on space groups
+ge = csvRead("./data/ge/ge"+str(SPACE)+".csv", ['ITA', 'rot', 'trans'])
+wp = csvRead("./data/wp/wp"+str(SPACE)+".csv", ['Mult', 'Letter', 'Symm', 'Pos'])
+
+rots, trans=[], []
+for x in ge:
+    rots.append(Matrix(sympify(x[1])))
+    trans.append(Matrix(sympify(x[2])))
+
+pos, ptitle=[],[]
+i=0
+for x in wp:
+    ptitle.append(x[0]+x[1])
+    pos.append(i)
+    pos[i]=[]
+    for y in x[3].split(', '):
+        z=y.replace('[M','M').replace(')]',')') # Removes left over braces
+        pos[i].append(Matrix(sympify(z)))
+    i+=1
+
+for x in pos:
+    print x
+    print
